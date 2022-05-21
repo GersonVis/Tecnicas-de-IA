6af4ab305e3861b247abed8278c7abfdf568c39d
GersonVis <visoso126@gmail.com>
2022-05-21 10:01:02 -0500

    first commit

---
+++ archivo.py
@@ -0,0 +1,10 @@
+def sumar_dos_numeros(primer_numero:int, segundo_numero:int)->int:
+    """
+    permite sumar dos numeros enteros
+    Args:
+       primer_numero (int): N1
+       segundo_numero (int): N2
+    Returns:
+       int: la suma de primer_numero y segundo numero
+    """
+    return primer_numero+segundo_numero
